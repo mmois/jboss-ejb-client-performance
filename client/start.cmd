cf5e51123d7844c8609db533c7541d56205a3371
mmois <martin.mois@googlemail.com>
2013-04-07 13:26:17 +0200

    modified .gitignore

--- client/start.cmd
+++ client/start.cmd
@@ -1,3 +1,3 @@
 @echo off
-java -jar target\client-0.0.1-SNAPSHOT-jar-with-dependencies.jar 50 10 1
+java -Xms64M -Xmx2048M -XX:MaxPermSize=256M -jar target\client-0.0.1-SNAPSHOT-jar-with-dependencies.jar 50 5 2
 pause
